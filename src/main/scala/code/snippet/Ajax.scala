79d93ee07b902eb98db48229f7cd7ee4ad900487
Diego Medina (fmpwizard) <diego@fmpwizard.com>
2012-06-11 02:13:22 -0400

    * We had to escape the single quotes that ajaxCall was generating, because that was messing with the prepend JavaScript command * Added some js code to removeTag to remove the actual tag from the html page ** TODO: You can only add and remove one tag at the time, if you try to add two tags, the delete gets executed, but I leave that for the reader to fix.

--- src/main/scala/code/snippet/Ajax.scala
+++ src/main/scala/code/snippet/Ajax.scala
@@ -5,8 +5,8 @@ import net.liftweb._
 import http._
 import js.JsCmds._
 import js.JE._
-import js.JsCmd
 import common._
+import js.{JE, JsCmd}
 import util._
 import Helpers._
 import scala.xml._
@@ -28,14 +28,19 @@ class HelloWorld extends Logger {
 
 
 
-   def exec = "#tag_input" #> SHtml.onSubmit(str => { 
-      prepend("lift-tag-prepend", <div id={str+"tag"} class="tag"><span>{str+" (Tag)"}</span><span onclick={SHtml.ajaxCall(Str(str), removeTag _)._2.toJsCmd.toString+"; return false;"}>X</span></div>) &
-      SetValById("tag_input", "")
-    })
+   def exec = "#tag_input" #> SHtml.onSubmit(str => {
+     val deleteButton= <button onclick={SHtml.ajaxCall(Str(str), removeTag _)._2.toJsCmd +"; return false;"}>X</button>.toString().replace("'","\\'")
+     println("\n\nsssssssdddf\n\n" + deleteButton)
+     val node= xml.XML.loadString(
+       deleteButton
+     )
+     prepend("lift-tag-prepend", <div id={str+"tag"} class="tag"><span>{str+" (Tag)"}</span>{node}</div>) &
+     SetValById("tag_input", "")
+   })
 
    def removeTag(tag: String): JsCmd = {
     info("Remove Tag: "+tag)
-    Noop
+    JE.JsRaw("""$("#""" + tag +"""tag").remove()""").cmd
    }
    
    def prepend(id: String, node: Elem): JsCmd = JsRaw("$('#"+id+"').prepend('"+node+"');")
